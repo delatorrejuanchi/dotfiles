40a028c3acc8e0b42a9634bb82a2184314e9faec
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-09-27 16:10:08 -0300

    feat(nvim): configure obsidian for note taking synced via iCloud Drive

--- .config/LazyVim/lua/plugins/notes/obsidian.lua
+++ .config/LazyVim/lua/plugins/notes/obsidian.lua
@@ -1,5 +1,10 @@
 local prefix = "<leader>o"
-local vault = vim.fn.expand("~") .. "/obsidian/main"
+local vault = vim.fn.expand("~") .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/main"
+
+-- disable obsidian.nvim if not on MacOS
+if not vim.fn.has("macunix") then
+  return {}
+end
 
 return {
   {
@@ -17,6 +22,7 @@ return {
     keys = {
       { prefix .. "f", "<cmd>ObsidianQuickSwitch<CR>", desc = "Quick Switch" },
       { prefix .. "t", "<cmd>ObsidianToday<CR>", desc = "Today" },
+      { prefix .. "n", "<cmd>ObsidianNew<CR>", desc = "New note" },
     },
 
     opts = {
@@ -27,6 +33,47 @@ return {
 
       use_advanced_uri = true,
       overwrite_mappings = true,
+
+      completion = {
+        new_notes_location = "notes_subdir",
+      },
+
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+      end,
+
+      note_frontmatter_func = function(note)
+        local now = os.date("%Y-%m-%d %H:%M")
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        if out.created_at == nil then
+          out.created_at = now
+        end
+        out.updated_at = now
+
+        return out
+      end,
     },
   },
   {
