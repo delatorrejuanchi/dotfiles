e0ef6480d92e22aa4c22334bbe7d8f4a9a673d89
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-09-27 23:01:31 -0300

    feat(nvim): add markdown support

---
+++ .config/LazyVim/lua/plugins/lang/markdown.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "markdown",
+        "markdown_inline",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "marksman", "prettierd" })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      if type(opts.sources) == "table" then
+        local nls = require("null-ls")
+        vim.list_extend(opts.sources, { nls.builtins.formatting.prettierd })
+      end
+    end,
+  },
+}
