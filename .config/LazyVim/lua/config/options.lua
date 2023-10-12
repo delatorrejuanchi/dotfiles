a8c445e1d72f9d0d5d96c5034a6d230bdeb4999b
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-10-12 01:16:28 -0300

    feat(nvim): ignore mocks in lsp pickers

--- .config/LazyVim/lua/config/options.lua
+++ .config/LazyVim/lua/config/options.lua
@@ -2,4 +2,4 @@ vim.opt.cursorlineopt = "number" -- Only highlight number column
 vim.opt.fillchars = { eob = " " } -- Hide ~ at end of buffer
 vim.opt.laststatus = 2 -- Always show statusline
 
-vim.g.lsp_ignore_tests = true -- Ignore tests in lsp references/implementations
+vim.g.lsp_ignore_patterns_enabled = true -- Ignore tests in lsp references/implementations
--- .config/LazyVim/lua/plugins/editor/lspconfig.lua
+++ .config/LazyVim/lua/plugins/editor/lspconfig.lua
@@ -1,6 +1,8 @@
-local tests_ignore_patterns = {
+local lsp_ignore_patterns = {
   "[^a-z]test[^a-z]",
+  "[^a-z]mock[^a-z]",
   "Test[^a-z]",
+  "Mock[^a-z]",
 }
 
 return {
@@ -11,9 +13,9 @@ return {
     keys[#keys + 1] = {
       "<leader>ut",
       function()
-        vim.g.lsp_ignore_tests = not vim.g.lsp_ignore_tests
+        vim.g.lsp_ignore_patterns_enabled = not vim.g.lsp_ignore_patterns_enabled
       end,
-      desc = "Toggle lsp ignore pattern for tests",
+      desc = "Toggle lsp ignore pattern",
     }
 
     keys[#keys + 1] = {
@@ -21,7 +23,7 @@ return {
       function()
         require("telescope.builtin").lsp_references({
           reuse_win = true,
-          file_ignore_patterns = vim.g.lsp_ignore_tests and tests_ignore_patterns or {},
+          file_ignore_patterns = vim.g.lsp_ignore_patterns_enabled and lsp_ignore_patterns or {},
         })
       end,
       desc = "References",
@@ -32,7 +34,7 @@ return {
       function()
         require("telescope.builtin").lsp_implementations({
           reuse_win = true,
-          file_ignore_patterns = vim.g.lsp_ignore_tests and tests_ignore_patterns or {},
+          file_ignore_patterns = vim.g.lsp_ignore_patterns_enabled and lsp_ignore_patterns or {},
         })
       end,
       desc = "Implementations",
