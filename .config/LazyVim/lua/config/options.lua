7bfde7a422fe0267ef95ac2c027d16f43828fa4a
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-10-12 01:05:10 -0300

    feat(nvim): ignore tests in lsp pickers by default

--- .config/LazyVim/lua/config/options.lua
+++ .config/LazyVim/lua/config/options.lua
@@ -1,3 +1,5 @@
 vim.opt.cursorlineopt = "number" -- Only highlight number column
 vim.opt.fillchars = { eob = " " } -- Hide ~ at end of buffer
 vim.opt.laststatus = 2 -- Always show statusline
+
+vim.g.lsp_ignore_tests = true -- Ignore tests in lsp references/implementations
