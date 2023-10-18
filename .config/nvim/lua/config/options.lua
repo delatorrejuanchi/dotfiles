2a44ecc88bccdc52fda68156cd3eac942245ed95
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-10-18 10:27:09 -0300

    fix(nvim): disable comment continuation with autocmd

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,3 @@
 vim.opt.cursorlineopt = "number" -- Only highlight number column
 vim.opt.fillchars = { eob = " " } -- Hide ~ at end of buffer
 vim.opt.laststatus = 2 -- Always show statusline
-
-vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Disable comment continuation
