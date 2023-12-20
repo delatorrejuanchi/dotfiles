4fca59d4331c43a751a4cecfc4547d701272226d
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-12-20 01:17:03 -0300

    feat(nvim): bump haskell-tools to v3 and improve config

--- .config/nvim/lua/plugins/lang/haskell.lua
+++ .config/nvim/lua/plugins/lang/haskell.lua
@@ -1,31 +1,82 @@
+-- NOTE: Use LazyVim's extra when the https://github.com/LazyVim/LazyVim/pull/2052 is merged
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
 
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "haskell" })
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "haskell" })
+      end
     end,
   },
   {
     "mrcjkb/haskell-tools.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    version = "^2",
+    version = "^3",
     ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+    dependencies = {
+      { "nvim-telescope/telescope.nvim", optional = true },
+    },
 
     config = function()
+      local ok, telescope = pcall(require, "telescope")
+      if ok then
+        telescope.load_extension("ht")
+      end
+
       vim.g.haskell_tools = {
         hls = {
           settings = {
             haskell = {
               plugin = {
-                stan = { globalOn = false },
+                stan = { globalOn = true },
               },
             },
           },
         },
       }
     end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      { "mrcjkb/neotest-haskell" },
+    },
+
+    opts = {
+      adapters = {
+        ["neotest-haskell"] = {},
+      },
+    },
+  },
+  {
+    "luc-tielen/telescope_hoogle",
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+    },
+
+    config = function()
+      local ok, telescope = pcall(require, "telescope")
+      if ok then
+        telescope.load_extension("hoogle")
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+
+    opts = {
+      setup = {
+        hls = function() -- ensure that lspconfig doesn't start hls since it might conflitct with haskell-tools
+          return true
+        end,
+      },
+    },
   },
 }
