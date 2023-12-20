6a088164a4bf78cfd40bfc273443da11a8cb2ebf
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-12-20 00:56:43 -0300

    feat(nvim): configure ocaml lsp

--- .config/nvim/lua/plugins/lang/ocaml.lua
+++ .config/nvim/lua/plugins/lang/ocaml.lua
@@ -7,19 +7,24 @@ return {
     end,
   },
   {
-    "williamboman/mason.nvim",
-
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "ocaml-lsp", "ocamlformat" })
-    end,
-  },
-  {
     "stevearc/conform.nvim",
 
     opts = {
       formatters_by_ft = {
         ocaml = { "ocamlformat" },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ocamllsp = {
+          mason = false,
+          settings = {
+            codelens = { enable = true },
+          },
+        },
       },
     },
   },
