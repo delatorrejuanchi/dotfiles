86152e14c088687793a16ece838d895c9b7a22e1
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-12-20 01:01:41 -0300

    feat(nvim): remove markdown and add rust from extras

--- .config/nvim/lua/plugins/lang/extras.lua
+++ .config/nvim/lua/plugins/lang/extras.lua
@@ -1,6 +1,6 @@
 return {
+  { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
