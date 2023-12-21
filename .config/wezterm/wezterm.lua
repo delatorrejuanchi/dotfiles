9b12ec379d5c98ff6d58128c7b271ab539e65b0c
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-12-21 16:37:14 -0300

    feat: start using wezterm, remove kitty config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "Tokyo Night Storm"
+config.font = wezterm.font("SFMono Nerd Font")
+config.font_size = 12
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 2
+config.hide_tab_bar_if_only_one_tab = true
+
+config.disable_default_key_bindings = false
+config.adjust_window_size_when_changing_font_size = false
+config.allow_win32_input_mode = false
+config.window_close_confirmation = "NeverPrompt"
+
+return config
