77a5423c888b3676639eeb42203a7d14d266bef3
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-12-24 20:50:25 -0300

    feat(nvim): configure filetypes for yabairc and skhdrc

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,10 @@ vim.opt.fillchars = { eob = " " } -- Hide ~ at end of buffer
 vim.opt.laststatus = 2 -- Always show statusline
 vim.opt.pumblend = 0 -- Popup menu transparency
 vim.opt.conceallevel = 2 -- Conceal text
+
+vim.filetype.add({
+  filename = {
+    ["yabairc"] = "sh",
+    ["skhdrc"] = "config",
+  },
+})
