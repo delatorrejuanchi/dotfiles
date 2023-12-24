4da58d11c795a201ceafc63aec87cac583b2caa9
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-12-24 20:51:33 -0300

    feat(nvim): stop using copilot as a cmp source

--- .config/nvim/lua/plugins/coding/copilot.lua
+++ .config/nvim/lua/plugins/coding/copilot.lua
@@ -1,6 +1,21 @@
 return {
-  import = "lazyvim.plugins.extras.coding.copilot",
-  cond = function()
-    return vim.fn.getcwd():find("/Volumes/code/") ~= 1
-  end,
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "BufEnter",
+  build = ":Copilot auth",
+
+  ---@type copilot_config
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = vim.fn.getcwd():find("/Volumes/code/") ~= 1,
+    },
+    panel = { enabled = false },
+    filetypes = {
+      yaml = true,
+      markdown = true,
+      help = true,
+      gitcommit = true,
+    },
+  },
 }
