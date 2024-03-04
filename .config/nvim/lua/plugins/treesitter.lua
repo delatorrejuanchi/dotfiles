a4c8dc76af365f44f7b296ec5c38ed37fa2a6741
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2024-03-04 15:17:08 -0300

    feat(nvim): add treesitter textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+  },
+
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   event = { "BufReadPre" },
 
@@ -15,6 +19,23 @@ return {
 
     highlight = { enable = true },
     indent = { enable = true },
+    textobjects = {
+      select = {
+        enable = true,
+
+        lookahead = true,
+
+        keymaps = {
+          ["af"] = { query = "@function.outer", desc = "around function" },
+          ["if"] = { query = "@function.inner", desc = "inside function" },
+
+          ["aa"] = { query = "@parameter.outer", desc = "around argument" },
+          ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+        },
+
+        include_surrounding_whitespace = true,
+      },
+    },
   },
 
   config = function(_, opts)
