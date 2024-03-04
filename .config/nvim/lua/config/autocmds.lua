13323722e9c77eb6e3eac919e27f7080a36e1659
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2024-03-04 15:15:42 -0300

    feat(nvim): add resize and highlight autocmds

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,13 @@
+-- highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
