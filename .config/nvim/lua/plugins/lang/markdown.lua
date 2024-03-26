d3f5d896029d4ce93e6341c8d74a382f5738e70e
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2024-03-26 20:07:44 -0300

    feat(nvim): remove markdownlint linter

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -10,26 +10,6 @@ return {
     },
   },
   {
-    "mfussenegger/nvim-lint",
-    optional = true,
-
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-
-        opts = function(_, opts)
-          opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, { "markdownlint" })
-        end,
-      },
-    },
-
-    opts = {
-      linters_by_ft = {
-        markdown = { "markdownlint" },
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
     optional = true,
 
