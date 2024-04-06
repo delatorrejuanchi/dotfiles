9e397ca143b4f1d1b8ec3fe87f2c54f082f4b987
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-04-06 20:02:28 -0300

    feat(nvim): add meh extra

---
+++ .config/nvim/lua/plugins/extras/meh.lua
@@ -0,0 +1,40 @@
+local meh_1 = "<C-M-!>"
+local meh_2 = "<C-M-@>"
+
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+
+    keys = {
+      {
+        meh_1,
+        mode = "i",
+        function()
+          require("cmp").mapping.confirm({ select = true })
+        end,
+        desc = "confirm completion",
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    optional = true,
+
+    keys = {
+      {
+        meh_2,
+        mode = "i",
+        function()
+          local suggestion = require("copilot.suggestion")
+
+          if suggestion.is_visible() then
+            suggestion.accept()
+          else
+            suggestion.next()
+          end
+        end,
+      },
+    },
+  },
+}
