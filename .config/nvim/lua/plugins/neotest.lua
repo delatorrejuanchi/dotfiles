5145d7031f427d33a5ec11e205df9963b344a82e
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-04-08 22:32:55 -0300

    feat(nvim): remove testing extra in favor of an explicit neotest config

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/nvim-nio",
+    {
+      "folke/which-key.nvim",
+      optional = true,
+
+      opts = {
+        defaults = {
+          ["<leader>t"] = { name = "+test" },
+        },
+      },
+    },
+  },
+
+  keys = {
+      -- stylua: ignore start
+      { "<leader>tr", function() require("neotest").run.run() end, desc = "run test" },
+      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "run tests in file" },
+      { "<leader>ta", function() require("neotest").run.run({ suite = true }) end, desc = "run all tests" },
+      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "run last test" },
+
+      { "<leader>tx", function() require("neotest").run.stop() end, desc = "stop tests"},
+
+      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "toggle summary" },
+      { "<leader>to", function() require("neotest").output_panel.toggle() end, desc = "toggle output panel" },
+  },
+
+  opts = {
+    output = { enabled = false },
+    output_panel = { enabled = true },
+    quickfix = { enabled = true },
+
+    running = {
+      concurrent = true,
+    },
+  },
+}
