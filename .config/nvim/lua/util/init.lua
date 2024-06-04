f5336a8b1a61ee135684ff074a2e2da63c542d95
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-06-04 16:47:39 -0300

    feat(nvim): add util.create_undo

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -32,4 +32,11 @@ function M.debounce(ms, fn)
   end
 end
 
+M.CREATE_UNDO = vim.api.nvim_replace_termcodes("<c-G>u", true, true, true)
+function M.create_undo()
+  if vim.api.nvim_get_mode().mode == "i" then
+    vim.api.nvim_feedkeys(M.CREATE_UNDO, "n", false)
+  end
+end
+
 return M
