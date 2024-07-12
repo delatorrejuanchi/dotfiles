93a48e3589b5745ed4897da01fb7be6d49cde9d5
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-07-12 17:52:25 -0300

    feat(nvim): add smart-splits keybinds to resize panes

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -5,10 +5,15 @@ return {
 
   keys = {
     -- stylua: ignore start
-    { "<C-Left>", function() require("smart-splits").move_cursor_left() end, desc = "Move cursor left"},
+    { "<C-Left>", function() require("smart-splits").move_cursor_left() end, desc = "Move cursor left" },
     { "<C-Down>", function() require("smart-splits").move_cursor_down() end, desc = "Move cursor down" },
     { "<C-Up>", function() require("smart-splits").move_cursor_up() end, desc = "Move cursor up" },
     { "<C-Right>", function() require("smart-splits").move_cursor_right() end, desc = "Move cursor right" },
+
+    { "<C-S-Left>", function () require("smart-splits").resize_left() end, desc = "Resize left" },
+    { "<C-S-Down>", function () require("smart-splits").resize_down() end, desc = "Resize down" },
+    { "<C-S-Up>", function () require("smart-splits").resize_up() end, desc = "Resize up" },
+    { "<C-S-Right>", function () require("smart-splits").resize_right() end, desc = "Resize right" },
   },
 
   opts = {
