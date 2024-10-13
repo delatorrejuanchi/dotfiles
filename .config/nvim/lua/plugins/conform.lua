b3b9d4d0f881ea8a57e91d7660b34b7fc07e2f57
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-10-13 19:55:48 -0300

    fix(nvim): update conform configuration for lsp fallback

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -7,6 +7,6 @@ return {
 
   opts = {
     notify_on_error = false,
-    format_after_save = { timeout_ms = 5000, lsp_fallback = true },
+    format_after_save = { timeout_ms = 5000, lsp_format = "fallback" },
   },
 }
