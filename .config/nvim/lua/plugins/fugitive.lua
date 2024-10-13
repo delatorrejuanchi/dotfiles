4b0aa31fe13b1a7585402decff4a1187178312b8
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-10-13 19:55:06 -0300

    chore(nvim): remove unused fugitive keymaps

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -4,11 +4,6 @@ return {
     dependencies = { "tpope/vim-rhubarb" },
 
     keys = {
-      { "<leader>ga.", "<cmd>Git add %<CR>", mode = { "n" }, desc = "buffer" },
-      { "<leader>gaa", "<cmd>Git add .<CR>", mode = { "n" }, desc = "all" },
-      { "<leader>gc", "<cmd>Git commit<CR>", mode = { "n" }, desc = "commit" },
-      { "<leader>gP", "<cmd>Git push<CR>", mode = { "n" }, desc = "push" },
-      { "<leader>gp", "<cmd>Git pull<CR>", mode = { "n" }, desc = "pull" },
       { "<leader>gy", "<cmd>GBrowse!<CR>", mode = { "n" }, desc = "yank permalink" },
       { "<leader>gy", "<esc><cmd>'<,'>GBrowse!<CR>", mode = { "v" }, desc = "yank permalink" },
     },
