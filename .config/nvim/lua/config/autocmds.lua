393e717d779e6846308f948777328e980bdc3e13
Juan Cruz De La Torre <delatorrejuanchi@delatorrejuanchi.local>
2024-12-14 11:02:39 -0300

    fix(nvim): checktime autocmd on current buffer only

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -71,9 +71,9 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "CursorMoved", "CursorMovedI" }, {
 
 -- check if we need to reload the file
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave", "CursorHold", "CursorHoldI" }, {
-  callback = function()
+  callback = function(event)
     if vim.o.buftype ~= "" then
-      vim.cmd("checktime")
+      vim.cmd(event.buf .. "checktime")
     end
   end,
 })
